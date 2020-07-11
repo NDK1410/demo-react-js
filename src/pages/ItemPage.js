4adb45a189b89c132116ee9f5c519c96703e95cf
NDK1410 <khanh.nd1410@gmail.com>
2020-07-11 18:23:31 +0700

    setup-json-server-and-routes

---
+++ src/pages/ItemPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ItemPage extends Component {
+	render() {
+		return (
+			<div className="ItemPage">
+				<h1>Items Page</h1>
+			</div>
+		)
+	}
+}
+
+export default ItemPage;
