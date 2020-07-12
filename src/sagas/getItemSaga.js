89a28529260b21b654b664bf6861f2a55a236522
NDK1410 <khanh.nd1410@gmail.com>
2020-07-12 10:25:08 +0700

    delete-data

--- src/sagas/getItemSaga.js
+++ src/sagas/getItemSaga.js
@@ -1,6 +1,7 @@
 import { put, takeEvery } from "redux-saga/effects"
 import getItems from "../fetchAPI/getItems"
 import addItems from "../fetchAPI/addItems"
+import deleteItems from "../fetchAPI/deleteItems"
 import * as types from "../contants"
 
 function* getListItem(action) {
@@ -39,8 +40,28 @@ function* addItemActions(action) {
 	}
 }
 
+function* deleteItemActions(action) {
+	try {
+		yield deleteItems(action.payload);
+		yield put ({
+			type: types.DELETE_ITEM_SUCCESS
+		})
+		yield put ({
+			type: types.GET_ITEM_REQUEST
+		})
+	} catch (error) {
+		yield put ({
+			type: types.DELETE_ITEM_FAILURE,
+			payload: {
+				errorMessage: error.message
+			}
+		})
+	}
+}
+
 
 export const getItemSaga = [
     takeEvery(types.GET_ITEM_REQUEST, getListItem),
-    takeEvery(types.ADD_ITEM_REQUEST, addItemActions)
+	takeEvery(types.ADD_ITEM_REQUEST, addItemActions),
+	takeEvery(types.DELETE_ITEM_REQUEST, deleteItemActions)
 ] 
