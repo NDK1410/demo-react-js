496c0e246955fdc5c05bf90428c8eee5b549563c
NDK1410 <khanh.nd1410@gmail.com>
2020-07-12 10:00:53 +0700

    create-data

--- src/sagas/getItemSaga.js
+++ src/sagas/getItemSaga.js
@@ -1,5 +1,6 @@
 import { put, takeEvery } from "redux-saga/effects"
 import getItems from "../fetchAPI/getItems"
+import addItems from "../fetchAPI/addItems"
 import * as types from "../contants"
 
 function* getListItem(action) {
@@ -19,6 +20,27 @@ function* getListItem(action) {
 	}
 }
 
+function* addItemActions(action) {
+	try { 
+		yield addItems(action.payload);
+		yield put({
+			type: types.ADD_ITEM_SUCCESS
+		})
+		yield put({
+			type: types.GET_ITEM_REQUEST
+		})
+	} catch (error) {
+		yield put({
+			type: types.ADD_ITEM_FAILURE,
+			payload: {
+				errorMessage: error.message
+			}
+		})
+	}
+}
+
+
 export const getItemSaga = [
-	takeEvery(types.GET_ITEM_REQUEST, getListItem)
+    takeEvery(types.GET_ITEM_REQUEST, getListItem),
+    takeEvery(types.ADD_ITEM_REQUEST, addItemActions)
 ] 
