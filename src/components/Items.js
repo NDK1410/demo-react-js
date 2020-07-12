a8923949e97df30b6879afda2bdd8ad69619e9c9
NDK1410 <khanh.nd1410@gmail.com>
2020-07-12 18:24:34 +0700

    pagination

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+
 class Items extends Component {
 	constructor() {
 		super();
 		this.state = {
+			currentPage: 1,
+			perPage: 3,
 			inputAdd: "",
 			objUpdate: {
 				id: 0,
@@ -12,33 +15,52 @@ class Items extends Component {
 		}
 	}
 
+	chosePage = (event) => {
+		this.setState({
+			currentPage: Number(event.target.id)
+		});
+	}
+
+	select = (event) => {
+		this.setState({
+			perPage: event.target.value
+		})
+	}
 
 	render() {
 		let listData = []
-		if (this.props.items) {
-			listData = this.props.items.map((item, key) => {
-				return (
-					<tr key={key}>
-						<th>{item.id}</th>
-						<th>{item.name}</th>
-						<th>
-							<button onClick={
-								() => {
-									this.props.deleteDispatch({dataDelete: item.id, textSearch: this.props.textSearch})
-								}
-							}> Delete </button>
-						</th>
+		const currentPage = this.state.currentPage;
+		const perPage = this.state.perPage;
+		const indexOfLastNews = currentPage * perPage;
+		const indexOfFirstNews = indexOfLastNews - perPage;
+		const currentTodos = listData.slice(indexOfFirstNews, indexOfLastNews);
+		const renderTodos = currentTodos.map((item, key) => {
+			return (
+				<tr key={key}>
+					<th>{item.id}</th>
+					<th>{item.name}</th>
+					<th>
+						<button onClick={
+							() => {
+								this.props.deleteDispatch({ dataDelete: item.id, textSearch: this.props.textSearch })
+							}
+						}> Delete </button>
+					</th>
 
-						<th>
-							<button onClick={(e) => { this.setState({ objUpdate: item }) }}> Select </button>
-						</th>
-					</tr>
-				)
-			})
+					<th>
+						<button onClick={(e) => { this.setState({ objUpdate: item }) }}> Select </button>
+					</th>
+				</tr>
+			)
+		});
+		const pageNumbers = [];
+		for (let i = 1; i <= Math.ceil(listData.length / perPage); i++) {
+			pageNumbers.push(i);
 		}
 		return (
 			<div>
 				<div>
+
 					<input value={this.state.textSearch} onChange={
 						(e) => {
 							this.setState({
@@ -64,6 +86,7 @@ class Items extends Component {
 					}
 				}> Add </button>
 
+
 				<div>
 					<input value={this.state.objUpdate.name} onChange={
 						(event) => {
@@ -78,7 +101,7 @@ class Items extends Component {
 
 					<button onClick={
 						() => {
-							this.props.updateDispatch({dataUpdate: this.state.objUpdate, textSearch: this.props.textSearch})
+							this.props.updateDispatch({ dataUpdate: this.state.objUpdate, textSearch: this.props.textSearch })
 						}
 					}>  Edit </button>
 				</div>
@@ -89,9 +112,41 @@ class Items extends Component {
 							<th>ID</th>
 							<th>Name</th>
 						</tr>
-						{listData}
+						{renderTodos}
 					</tbody>
+					<div className="news-per-page">
+						<select defaultValue="0" onChange={this.select} >
+							<option value="0" disabled>Get by</option>
+							<option value="3">3</option>
+							<option value="5">5</option>
+							<option value="7">7</option>
+						</select>
+					</div>
 				</table>
+
+				<div className="pagination-custom">
+					<ul id="page-numbers">
+						{	
+							pageNumbers.map(number => {
+								if (this.state.currentPage === number) {
+									return (
+										<li key={number} id={number} className="active">
+											{number}
+										</li>
+									)
+								}
+								else {
+									return (
+										<li key={number} id={number} onClick={this.chosePage} >
+											{number}
+										</li>
+									)
+								}
+							})
+						}
+					</ul>
+				</div>
+
 			</div>
 		);
 	}
