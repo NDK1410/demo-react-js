f8f9862a74726cc07934c2ea301dddb1030763e1
NDK1410 <khanh.nd1410@gmail.com>
2020-07-18 00:38:07 +0700

    Updates

---
+++ precache-manifest.1a06836f3db8304f7a5f51a009314734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea03b852fb77bd7ed2f5fc997a8f16c3",
+    "url": "/demo-react-js/index.html"
+  },
+  {
+    "revision": "ace04e53df0bbecd3f1b",
+    "url": "/demo-react-js/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "0fed9afcd16b0bf9bb1b",
+    "url": "/demo-react-js/static/js/2.a1a6d858.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-react-js/static/js/2.a1a6d858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace04e53df0bbecd3f1b",
+    "url": "/demo-react-js/static/js/main.b8f197bd.chunk.js"
+  },
+  {
+    "revision": "b862f95d39612e0697b0",
+    "url": "/demo-react-js/static/js/runtime-main.07f57872.js"
+  }
+]);
